c29edba808c45d755b94d86f26724744f7de709d
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-04-11 12:29:14 +0900

    サイン波の生成

---
+++ example1.py
@@ -0,0 +1,10 @@
+from synthesizer import Synthesizer, Waveform, Writer
+
+synth = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
+
+# 一定音程の波形を生成する
+wave = synth.generate_constant_wave(frequency=440.0, length=1.0)
+
+# オーディオファイル出力用クラス
+writer = Writer()
+writer.write_wave("sine.wav", wave)
